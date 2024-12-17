b040c0e56c36adbd672c40c11003dc2035c1097a
Bhanuprakash-340 <bhanu150940@gmail.com>
2024-12-17 14:43:30 +0530

    added h2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <div>
        <h1> DEVELOP BRANCH</h1>
        <h2>pushing code into the develop branch 1st commit </h2>
+       <h2>pushing code into the develop branch 2nd commit SDE/2 </h2>
        </div>
     </>
   )
