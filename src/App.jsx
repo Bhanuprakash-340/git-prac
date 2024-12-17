2389c4ef36a5de88c7536cabd7154c4162bf9b03
Bhanuprakash-340 <bhanu150940@gmail.com>
2024-12-17 14:39:13 +0530

    code changes sde/1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        DEVELOP BRANCH
+       <h1> DEVELOP BRANCH</h1>
+       <h2>pushing code into the develop branch 1st commit </h2>
        </div>
     </>
   )
